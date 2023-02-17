aafe236e683386098a5946a5a03bd77d5a034523
OphelieThomas33 <ophelie_thomas@hotmail.com>
2023-02-17 18:25:08 +0100

    add routing system

---
+++ js/router.js
@@ -0,0 +1,36 @@
+const route = (event) => {
+    event = event || window.event;
+    event.preventDefault();
+    window.history.pushState({}, "", event.target.href);
+    handleLocation();
+};
+
+const routes = {
+    404: "/pages/404.html",
+    "/": "/pages/home.html",
+    "/testApi": "/pages/testApi.html",
+    "/replays": "/pages/replays.html",
+    "/categories": "/pages/categories.html"
+};
+
+const routesJS = {
+    "/testApi": "js/testApi.js",
+    "/replays": "js/replays.js",
+    "/categories": "js/categories.js"
+};
+
+const handleLocation = async () => {
+    const path = window.location.pathname;
+    const route = routes[path] || routes[404];
+    //Ajout de l'html
+    const html = await fetch(route).then((data) => data.text());
+    document.getElementById("main-page").innerHTML = html;
+    // Ajout du JS
+    if(routesJS[path] != undefined){
+        let scriptTag = document.createElement('script');
+        scriptTag.setAttribute("type","text/javascript");
+        scriptTag.setAttribute("src", routesJS[path]);
+
+        document.querySelector("body").appendChild(scriptTag);
+    }
+}
--- readme.md
+++ readme.md
@@ -1,9 +1,22 @@
-Techno :
-Vanilla JS -> JS sans librairie, sans framework
+# Projet front appelant une api 01/2023 #
+
+## Cahier des charges ##
+Application de gestion de replays (CRUD de liens vers des replays STUDI)
+-> Gérer des playlists
+-> Gestion des liens
+
+## Techno ##
+Vanilla JS -> Que du JS sans librairie ni framework
 SASS
 
+## SYSTEME DE ROUTING ##
+Pour ajouter une page, il faut ajouter le fichier html dans le dossier pages
+Ensuite, il faut configurer la route dans le fichier router.js (la variable route)
+Il faut ajouter un ligne suivant ce fonctionnement : 
+principe : "/route": "/pages/monFichier.html"
+exemple  : "/route": "pathToFileHtml"
 
-Application de gestion de replays (CRUD de liens vers des replays Studi)
--> gestion des playlists
--> gestion des liens
-
+Pour inlcure un fichier js dans cette route, il faut modifier la variable routeJS du fichier router.js
+Il faut ajouter un ligne suivant ce fonctionnement : 
+principe : "/route": "js/monFichier.js"
+exemple  : "/route": "pathToFilejs"
