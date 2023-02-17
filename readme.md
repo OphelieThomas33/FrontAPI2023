c4a88ac22e5375a1ce9f13a6c211fa79b479c926
OphelieThomas33 <ophelie_thomas@hotmail.com>
2023-02-17 13:34:49 +0100

    begin front project -> set header

---
+++ readme.md
@@ -0,0 +1,9 @@
+Techno :
+Vanilla JS -> JS sans librairie, sans framework
+SASS
+
+
+Application de gestion de replays (CRUD de liens vers des replays Studi)
+-> gestion des playlists
+-> gestion des liens
+
--- script.js
+++ script.js
@@ -2,54 +2,70 @@
 // URL: https://reqres.in/api/users
 // Method : GET
 
-const allEntriesDiv = document.getElementById('allEntries')
-const baseURL = 'https://reqres.in/'
-const baseURLUser = baseURL + 'api/users'
+// const allEntriesDiv = document.getElementById('allEntries')
+// const baseURL = 'https://reqres.in/'
+// const baseURLUser = baseURL + 'api/users'
 
-document.getElementById("getUsers").addEventListener('click', getAllUser)
+// document.getElementById("getUsers").addEventListener('click', getAllUser)
 
-function getAllUser() {
+// function getAllUser() {
     
-    let numPage = document.getElementById('pageNum').value
-    fetch(baseURLUser + '?' + new URLSearchParams({
-            per_page: 2,
-            page: numPage
-        }), 
-        {method: 'GET'}) // je fais la promesse de t'apporter quelque chose
-    .then(function(response){         // je te donne les données qu'on appelle ici response
-        // reponse reçue en texte, on transforme les données en json
-        return response.json()
-    })
-    .then(function(responseJson) {    // création d'une fonction avec les données en json
-        allEntriesDiv.innerHTML = ""
-        // utilisation des datas :
-        let allUserArray = responseJson.data;
-        allUserArray.forEach(user => {     // ici data est le nom des données dans CETTE API
-            // ou directement enlever le "let" et indiquer responseJson.data.forEach
-            allEntriesDiv.innerHTML += user.email + '<br>'    
-        })
-    })
+//     let numPage = document.getElementById('pageNum').value
+//     fetch(baseURLUser + '?' + new URLSearchParams({
+//             per_page: 2,
+//             page: numPage
+//         }), 
+//         {method: 'GET'}) // je fais la promesse de t'apporter quelque chose
+//     .then(function(response){         // je te donne les données qu'on appelle ici response
+//         // reponse reçue en texte, on transforme les données en json
+//         return response.json()
+//     })
+//     .then(function(responseJson) {    // création d'une fonction avec les données en json
+//         allEntriesDiv.innerHTML = ""
+//         // utilisation des datas :
+//         let allUserArray = responseJson.data;
+//         allUserArray.forEach(user => {     // ici data est le nom des données dans CETTE API
+//             // ou directement enlever le "let" et indiquer responseJson.data.forEach
+//             allEntriesDiv.innerHTML += user.email + '<br>'    
+//         })
+//     })
+// }
+
+// function createUser() {
+//     let name = document.getElementById("nomInput").value;
+//     let job = document.getElementById("jobInput").value;
+
+//     fetch(baseURLUser, {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify({
+//             "name": name,
+//             "job": job       
+//         })
+//     })
+//     .then(function(response){
+//         return response.json()
+//     })
+//     .then(function(responseJson){
+//         let dateCreation = new Date(responseJson.createdAt)
+//         alert("Félicitations, l'utilisateur n°" + responseJson.id + " a bien été créé le " + dateCreation.toLocaleDateString() + " - Nom : " + responseJson.name + " - job : " + responseJson.job)
+//     })
+// }
+
+// Selection of HTML objects
+const burger = document.querySelector('.burger i');
+const nav = document.querySelector('.nav');
+
+// Defining a function
+function toggleNav() {
+    burger.classList.toggle('fa-bars');
+    burger.classList.toggle('fa-times');
+    nav.classList.toggle('nav-active');
 }
 
-function createUser() {
-    let name = document.getElementById("nomInput").value;
-    let job = document.getElementById("jobInput").value;
-
-    fetch(baseURLUser, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            "name": name,
-            "job": job       
-        })
-    })
-    .then(function(response){
-        return response.json()
-    })
-    .then(function(responseJson){
-        let dateCreation = new Date(responseJson.createdAt)
-        alert("Félicitations, l'utilisateur n°" + responseJson.id + " a bien été créé le " + dateCreation.toLocaleDateString() + " - Nom : " + responseJson.name + " - job : " + responseJson.job)
-    })
-}
+// Calling the function after click event occurs
+burger.addEventListener('click', function() {
+    toggleNav();
+});
